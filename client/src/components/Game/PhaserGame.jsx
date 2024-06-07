c8c8c1e30c572a86f6cfd2c24d91a0bcd7f4b0e0
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-06-06 19:56:47 -0600

    stuff like that

--- client/src/components/Game/PhaserGame.jsx
+++ client/src/components/Game/PhaserGame.jsx
@@ -10,6 +10,7 @@ class PhaserGame extends Phaser.Scene {
   preload() {
     // Load assets
     this.load.image('player', '/sprites/player/Ships/blue-1.png'); // Ensure the key matches in create()
+    this.load.image('enemy', '/sprites/player/Ships/Enemies/Enemies-1.png');
   }
 
   create() {
@@ -19,11 +20,24 @@ class PhaserGame extends Phaser.Scene {
     // Create player sprite
     this.player = this.physics.add.sprite(400, 300, 'player'); // Create player sprite directly
 
+    // Create enemy sprite
+    this.enemy = this.physics.add.sprite(1420, 400, 'enemy');
+
+    this.enemy.setVelocityX(900);
+
     // Define cursor keys for arrow key movement
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // Define WASD keys for movement
     this.keys = this.input.keyboard.addKeys('W,A,S,D');
+
+    this.setRandomVelocity(this.enemy);
+
+    this.time.addEvent({
+      delay: 2000, // 2 seconds
+      callback: () => this.setRandomVelocity(this.enemy),
+      loop: true
+    });
   }
 
   update() {
@@ -60,6 +74,16 @@ class PhaserGame extends Phaser.Scene {
 
   setCanMoveSprite(canMove) {
     this.canMoveSprite = canMove;
+  }
+
+  setRandomVelocity(sprite) {
+    const minVelocity = -100;
+    const maxVelocity = 100;
+
+    const randomXVelocity = Phaser.Math.Between(minVelocity, maxVelocity);
+    const randomYVelocity = Phaser.Math.Between(minVelocity, maxVelocity);
+
+    sprite.setVelocity(randomXVelocity, randomYVelocity);
   }
 }
 
